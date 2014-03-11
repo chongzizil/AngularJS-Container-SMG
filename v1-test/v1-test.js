3c9b1598e4a5a9e62c296c91f85a0029e0cc37ae
chongzizil <chongzizil@gmail.com>
2014-03-11 12:42:20 -0400

    HW5: Fix some typo and bugs

--- v1-test/v1-test.js
+++ v1-test/v1-test.js
@@ -5,51 +5,23 @@ var gamesUrl = "http://1.smg-server.appspot.com/container/gamesUrl";
 var nonExistUrl = "http://this-is-a-fake-url.nyu.edu";
 
 // assume that we only have two players with player id: 42 and 43.
-var sendOperations = '{"accessSignature": "?", "operations": "?"}';
+var startMatchWithGameId = '{"accessSignature": "...", "playerIds": "...", "gameId": "..."}';
+var joinMatchWithMatchId = '{"accessSignature": "...", "playerIds": "...", "matchId": "..."}';
+var makeMove = '{"accessSignature": "...", "SetTurn":"...", "operation":"..."}';
+var verifyMoveDone = '{"accessSignature": "...", "hackerPlayerId":"0", "message":"null"}';
 var Email = "chongzizil@gmail.com";
 var validPassword = "1234";
 var invalidPassword = "xxxxxxxxxx";
 var validPlayerId = "100";
 var invalidPlayerId = "101";
 var registerInfo = '{"email": "chongzizil@gmail.com", "password": "123456", "firstName": "Youlong", "lastName": "Li"}';
-var verifyMoveDone = '{"hackerPlayerId":"0", "message":"null"}';
-
-test("ReadyState or State Errors", function(){
-	var xhr;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xhr=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xhr=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xhr.open("POST", nonExistUrl, true);
-	xhr.responseType = 'json';
-	xhr.onreadystatechange = function(){
-		var status;
-		var data;
-		if(xhr.readyState == 4){
-			status = xhr.status;
-			if (status == 200){
-				data = xhr.response;
-				deepEqual(data, {"operations": "?"});
-			} else {
-				throws(
-						function(){
-							throw "error";
-						}, "xmlhttprequest do not have a right status!"
-				);
-			}
-		}
-	};
-	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send(sendOperations);
-});
 
 /* 
- * Start a new match
+ * Start(Insert) a new match
+ * Send the accessSignature, playerIds and gameId
+ * Should return matchId
  */
-test("Start a new match", function(){
+test("Start(Insert) a new match", function(){
 	var xhr;
 	if (window.XMLHttpRequest){
 		// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -67,7 +39,7 @@ test("Start a new match", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "matchId");
+				ok(data.has(matchId));
 			} else {
 				throws(
 						function(){
@@ -78,11 +50,49 @@ test("Start a new match", function(){
 		}
 	};
 	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send('{"accessSignature": "?", "playerIds": "?", "gameId": "?"}');
+	xhr.send(startMatchWithGameId);
+});
+
+/* 
+ * Join a match through a matchId
+ * Should return gameId and playerIds
+ */
+test("Join a match through a matchId", function(){
+	var xhr;
+	if (window.XMLHttpRequest){
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xhr=new XMLHttpRequest();
+	} else {
+		// code for IE6, IE5
+		xhr=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xhr.open("POST", matchesUrl, true);
+	xhr.responseType = 'json';
+	xhr.onreadystatechange = function(){
+		var status;
+		var data;
+		if(xhr.readyState == 4){
+			status = xhr.status;
+			if (status == 200){
+				data = xhr.response;
+				ok(data.has(gameId));
+				ok(data.has(playerIds));
+			} else {
+				throws(
+						function(){
+							throw "error";
+						}, "xmlhttprequest do not have a right status!"
+				);
+			}
+		}
+	};
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.send(joinMatchWithMatchId);
 });
 
 /* 
  * Receive the initial game infomation from the server
+ * Should return the game's url, name, pics, height and weight
  */
 test("Receive the initial game infomation from the server", function(){
 	var xhr;
@@ -102,11 +112,11 @@ test("Receive the initial game infomation from the server", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "url");
-				equal(typeof data[1], "name");
-				equal(typeof data[2], "pics");
-				equal(typeof data[3], "height");
-				equal(typeof data[4], "width");
+				ok(data.has(url));
+				ok(data.has(name));
+				ok(data.has(pics));
+				ok(data.has(height));
+				ok(data.has(width));
 			} else {
 				throws(
 						function(){
@@ -120,7 +130,8 @@ test("Receive the initial game infomation from the server", function(){
 });
 
 /* 
- * Send the player's move to the server and receive the UpdataUI
+ * Send the player's move to the server
+ * Should return UpdateUI
  */
 test("Send the player's move to the server", function(){
 	var xhr;
@@ -140,7 +151,14 @@ test("Send the player's move to the server", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "UpdataUI");
+				ok(data.has(yourPlayerId));
+				ok(data.has(playersInfo));
+				ok(data.has(state));
+				ok(data.has(UpdataUI));
+				ok(data.has(lastState));
+				ok(data.has(lastMove));
+				ok(data.has(lastMovePlayerId));
+				ok(data.has(playerIdToNumberOfTokensInPot));
 			} else {
 				throws(
 						function(){
@@ -151,45 +169,12 @@ test("Send the player's move to the server", function(){
 		}
 	};
 	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send(registerInfo);
-});
-
-/* 
- * Receive the verifyMove from the server
- */
-test("Receive the verifyMove from the server", function(){
-	var xhr;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xhr=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xhr=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xhr.open("GET", matchesUrl + "/matchId?verifyMove", true);
-	xhr.responseType = 'json';
-	xhr.onreadystatechange = function(){
-		var status;
-		var data;
-		if(xhr.readyState == 4){
-			status = xhr.status;
-			if (status == 200){
-				data = xhr.response;
-				equal(typeof data[0], "verifyMove");
-			} else {
-				throws(
-						function(){
-							throw "error";
-						}, "xmlhttprequest do not have a right status!"
-				);
-			}
-		}
-	};
-	xhr.send();
+	xhr.send(makeMove);
 });
 
 /* 
  * Send the verifyMoveDone to the server
+ * Should return UpdateUI
  */
 test("Send the verifyMoveDone to the server", function(){
 	var xhr;
@@ -209,7 +194,14 @@ test("Send the verifyMoveDone to the server", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "UpdateUI");
+				ok(data.has(yourPlayerId));
+				ok(data.has(playersInfo));
+				ok(data.has(state));
+				ok(data.has(UpdataUI));
+				ok(data.has(lastState));
+				ok(data.has(lastMove));
+				ok(data.has(lastMovePlayerId));
+				ok(data.has(playerIdToNumberOfTokensInPot));
 			} else {
 				throws(
 						function(){
@@ -225,6 +217,7 @@ test("Send the verifyMoveDone to the server", function(){
 
 /* 
  * Recieve the player of the current turn from the server
+ * Should return playerIdThatHasTurn
  */
 test("Recieve the player of the current turn from the server", function(){
 	var xhr;
@@ -244,7 +237,7 @@ test("Recieve the player of the current turn from the server", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "playerIdThatHasTurn");
+				ok(data.has(playerIdThatHasTurn));
 			} else {
 				throws(
 						function(){
@@ -259,8 +252,8 @@ test("Recieve the player of the current turn from the server", function(){
 
 /* 
  * Send a request to end game when tie to the server and receive the game over score and reason from the server
+ * Should return gameOverScores and gameOverReason
  */
-
 test("Send a request to end game when tie to the server and receive the game over score and reason from the server", function(){
 	var xhr;
 	if (window.XMLHttpRequest){
@@ -279,8 +272,8 @@ test("Send a request to end game when tie to the server and receive the game ove
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "gameOverScores");
-				equal(typeof data[1], "gameOverReason");
+				ok(data.has(gameOverScores));
+				ok(data.has(gameOverReason));
 			} else {
 				throws(
 						function(){
@@ -295,8 +288,8 @@ test("Send a request to end game when tie to the server and receive the game ove
 
 /* 
  * Send a request to end game when tie to the server and receive the rejection from the server
+ * Should return {"requestToEndTie" : "false"}
  */
-
 test("Send a request to end game when tie to the server and receive the game over score and reason from the server", function(){
 	var xhr;
 	if (window.XMLHttpRequest){
@@ -331,8 +324,8 @@ test("Send a request to end game when tie to the server and receive the game ove
 
 /* 
  * Send a cancel to end game and receive the game over score and reason from the server
+ * Should return gameOverScores and gameOverReason
  */
-
 test("Send a request to end game when tie to the server and receive the game over score and reason from the server", function(){
 	var xhr;
 	if (window.XMLHttpRequest){
@@ -351,8 +344,8 @@ test("Send a request to end game when tie to the server and receive the game ove
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "gameOverScores");
-				equal(typeof data[1], "gameOverReason");
+				ok(data.has(gameOverScores));
+				ok(data.has(gameOverReason));
 			} else {
 				throws(
 						function(){
@@ -367,6 +360,7 @@ test("Send a request to end game when tie to the server and receive the game ove
 
 /* 
  * Receive the game over score and reason from the server
+ * Should return gameOverScores and gameOverReason
  */
 test("Receive the game over score and reason from the server", function(){
 	var xhr;
@@ -386,8 +380,8 @@ test("Receive the game over score and reason from the server", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "gameOverScores");
-				equal(typeof data[1], "gameOverReason");
+				ok(data.has(gameOverScores));
+				ok(data.has(gameOverReason));
 			} else {
 				throws(
 						function(){
@@ -402,6 +396,7 @@ test("Receive the game over score and reason from the server", function(){
 
 /* 
  * Get a player's infomation from the server
+ * Should return playerId, name, nickName, pic, tokens
  */
 test("Get a player's infomation from the server", function(){
 	var xhr;
@@ -421,11 +416,11 @@ test("Get a player's infomation from the server", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "playerId");
-				equal(typeof data[1], "nickName");
-				equal(typeof data[2], "pic");
-				equal(typeof data[3], "tokens");
-				/*more player info*/
+				ok(data.has(playerId));
+				ok(data.has(name));
+				ok(data.has(nickName));
+				ok(data.has(pic));
+				ok(data.has(tokens));
 			} else {
 				throws(
 						function(){
@@ -435,47 +430,12 @@ test("Get a player's infomation from the server", function(){
 			}
 		}
 	};
-	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send('{"gameOverScores": "?", "gameOverReason": "?"}');
-});
-
-/* 
- * Request to find a player to match from the server
- */
-test("Request to find a player to match from the server", function(){
-	var xhr;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xhr=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xhr=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xhr.open("GET", matchesUrl + "gameId={gameId}&playerId={playerId}", true);
-	xhr.responseType = 'json';
-	xhr.onreadystatechange = function(){
-		var status;
-		var data;
-		if(xhr.readyState == 4){
-			status = xhr.status;
-			if (status == 200){
-				data = xhr.response;
-				equal(typeof data[0], "playerIds");
-			} else {
-				throws(
-						function(){
-							throw "error";
-						}, "xmlhttprequest do not have a right status!"
-				);
-			}
-		}
-	};
-	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send('{"gameOverScores": "?", "gameOverReason": "?"}');
+	xhr.send();
 });
 
 /* 
  * Register a new account
+ * Should return playerId
  */
 test("Register a new account", function(){
 	var xhr;
@@ -495,7 +455,7 @@ test("Register a new account", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "playerId");
+				ok(data.has(playerId));
 			} else {
 				throws(
 						function(){
@@ -511,6 +471,7 @@ test("Register a new account", function(){
 
 /* 
  * Register a new account with exists email
+ * Should return {"error": "EMAIL_EXISTS"}
  */
 test("Register a new account with exists email", function(){
 	var xhr;
@@ -546,6 +507,7 @@ test("Register a new account with exists email", function(){
 
 /* 
  * Login an account
+ * Should return playerId and accessSignature
  */
 test("Login an account", function(){
 	var xhr;
@@ -565,8 +527,8 @@ test("Login an account", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(typeof data[0], "playerId");
-				equal(typeof data[1], "accessSignature");
+				ok(data.has(playerId));
+				ok(data.has(accessSignature));
 			} else {
 				throws(
 						function(){
@@ -581,6 +543,7 @@ test("Login an account", function(){
 
 /* 
  * Login an account with a invalid playerId
+ * Should return {"error": "WRONG_PLAYER_ID"}
  */
 test("Login an account with a invalid playerId", function(){
 	var xhr;
@@ -615,6 +578,7 @@ test("Login an account with a invalid playerId", function(){
 
 /* 
  * Login an account with a invalid password
+ * Should return {"error": "WRONG_PASSWORD"}
  */
 test("Login an account with a invalid password", function(){
 	var xhr;
@@ -634,7 +598,6 @@ test("Login an account with a invalid password", function(){
 			status = xhr.status;
 			if (status == 200){
 				data = xhr.response;
-				equal(1,1);
 				deepEqual(data, {"error": "WRONG_PASSWORD"});
 			} else {
 				throws(
