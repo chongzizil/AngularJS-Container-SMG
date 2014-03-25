8ee18a3eb501f4efd774dcd174380f1d90524382
Long Yang <jasonyl0806@yahoo.com>
2014-03-25 14:24:22 -0400

    add reload button, and get matchInfo data from server

--- app/js/controllers/matchController.js
+++ app/js/controllers/matchController.js
@@ -1,26 +1,33 @@
 'use strict';
 
 smgContainer.controller('MatchController',
-		function ($scope, $routeParams, $rootScope, $cookies, $sce, MatchService, GetGameInfoService) {
+		function ($scope, $route, $routeParams, $rootScope, $cookies, $sce, MatchService, GetGameInfoService) {
 			var defaultGameUrl = "http://chongzizil.github.io";
 			// helper function used to get sce trusted url
 			var sceTrustedUrl = function(url){
 				return $sce.trustAsResourceUrl(url);
 			}
+			// object used to store all the information of the game
+			$scope.gameInfo = {};
 
-			$scope.game = {};
-			$scope.game.url = sceTrustedUrl(defaultGameUrl);
-			$scope.game.height = 400;
-			$scope.game.width = 400;
+			$scope.gameInfo.url = sceTrustedUrl(defaultGameUrl);
+			$scope.gameInfo.height = 400;
+			$scope.gameInfo.width = 400;
 
 			GetGameInfoService.get({gameId: $cookies.gameId}).
 					$promise.then(function(data) {
-						console.log('test--------test');
-						console.log(data);
-
-						$scope.url = sceTrustedUrl(data.url);
-						$scope.height = data.height;
-						$scope.width = data.width;
+						$scope.gameInfo = data;
 					}
 			);
+
+			MatchService.get({matchId: $routeParams.matchId}).
+					$promise.then(function(data){
+						$scope.matchInfo = data;
+					}
+			);
+
+			// method used to reload the match page to get the new match information for the server.
+			$scope.reload = function() {
+				$route.reload();
+			}
 });
