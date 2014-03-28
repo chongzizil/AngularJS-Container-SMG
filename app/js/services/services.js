9416dfbdbea8351b52b32ff8ec24848ad18791f8
Zil <chongzizil@gmail.com>
2014-03-27 21:43:11 -0400

    Added a new service for V2.

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -2,47 +2,47 @@
 
 /* Services */
 
+var domainUrl = 'http://1-dot-smg-server.appspot.com';
+
 smgContainer.factory('PlayerService', ['$resource', function($resource) {
-	return $resource('http://1.smg-server.appspot.com/players/:playerId',
+	return $resource(domainUrl + '/players/:playerId',
 		{playerId: '@playerId', password: '@password'}//,
 		//{register: {method:'POST', params:{}, headers:{'Content-Type': 'application/json'},  isArray:false}}
 		);
 }]);
 
 smgContainer.factory('GetGameInfoService', ['$resource', function($resource) {
-	return $resource('http://1.smg-server.appspot.com/games/:gameId',
+	return $resource(domainUrl + '/games/:gameId',
 			{gameId: '@gameId'}
 	);
 }]);
 
+smgContainer.factory('GetPlayerInfoService', ['$resource', function($resource) {
+	return $resource(domainUrl + '/playerInfo',
+			{playerId: '@playerId', targetId: '@targetId', accessSignature: '@accessSignature'}
+	);
+}]);
+
 smgContainer.factory('InsertMatchService', ['$resource', function($resource) {
-	return $resource('http://1.smg-server.appspot.com/newMatch',
+	return $resource(domainUrl + '/newMatch',
 			{}
 	);
 }]);
 
 smgContainer.factory('MatchService', ['$resource', function($resource) {
-	return $resource('http://1.smg-server.appspot.com/matches/:matchId',
+	return $resource(domainUrl + '/matches/:matchId',
 			{matchId: '@matchId', accessSignature: '@accessSignature', playerId: '@playerId'}
 	);
 }]);
 
 smgContainer.factory('DevService', ['$resource', function($resource) {
-	return $resource('http://1.smg-server.appspot.com/developers/:developerId',
+	return $resource(domainUrl + '/developers/:developerId',
 			{developerId: '@developerId', password: '@password'}
 	);
 }]);
 
 smgContainer.factory('UploadGameService', ['$resource', function($resource) {
-	return $resource('http://1.smg-server.appspot.com/games',
+	return $resource(domainUrl + '/games',
 			{}
 	);
 }]);
-
-/*
-smgContainer.factory('TestService', ['$resource', function($resource) {
-	return $resource('https://www.googleapis.com/urlshortener/v1/url',
-			{}
-	);
-}]);
-*/
