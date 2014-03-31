5b361759c682dfac29cc72d4c2e82c4c31d9a924
Long Yang <jasonyl0806@yahoo.com>
2014-03-31 16:00:34 -0400

    Finish MatchController interact with server part

--- app/js/controllers/matchController.js
+++ app/js/controllers/matchController.js
@@ -1,7 +1,8 @@
 //'use strict';
 
 smgContainer.controller('MatchController',
-		function ($scope, $route, $routeParams, $rootScope, $cookies, $sce, $window, MatchService, GetGameInfoService) {
+		function ($scope, $route, $routeParams, $rootScope, $cookies,
+		          $sce, $window, $location, MatchService, GetGameInfoService) {
 			if (!$cookies.accessSignature || !$cookies.playerId) {
 				alert('You have to log in first!');
 				$location.url('/');
@@ -9,91 +10,137 @@ smgContainer.controller('MatchController',
 				alert('You have to start a match by invite a friend or something...')
 				$location.url('/');
 			} else {
-				// helper function used to get sce trusted url
+				$scope.matchInfo = {};
+				var gameInfo = {};
+				var matchInfo = {};
+				var lastMovePlayerId;
+				var operations;
+				var sentMoveReceivedData;
+				var channelApiPushState;
+				var channel;
+				var socket;
+				var handler = {
+					onopen: function () { alert("onopen") },
+					onerror: function () { alert("onerror") },
+					onclose: function () { alert("onclose") },
+					onmessage:
+							function (event) {
+								console.log(event.data);
+								channelApiPushState = event.data['state'];
+								// We call 'Get' method to server to get the match information.
+								getMatchInfo();
+							}
+				};
+
+				/**
+				 * On match page start, match controller should get all the information for the game,
+				 * and create the changeApi channel to get the match information frequently.
+				 */
+				GetGameInfoService.get({gameId: $routeParams.gameId}).
+						$promise.then(function(data) {
+							if(data['error']) {
+								alert('You have provided WRONG_GAME_ID!');
+							} else {
+								console.log(data);
+								// 1. Get game information.
+								gameInfo.url = sceTrustedUrl(data['url']);
+								gameInfo.height = data['height'];
+								gameInfo.width = data['width'];
+								gameInfo.gameName = data['gameName'];
+								gameInfo.pics = data['pics'];
+								// 2. Create the channel to get match information.
+								channel = goog.appengine.Channel($cookies.channelToken);
+								socket = channel.open(handler);
+							}
+						}
+				);
+
+				/**
+				 * Method used to get match information data from server using 'GET' method.
+				 */
+				var getMatchInfo = function() {
+					MatchService.get({matchId: $routeParams.matchId,
+						accessSignature : $cookies.accessSignature, playerId : $cookies.playerId}).
+							$promise.then(function(data) {
+								if(data['error'] == "WRONG_ACCESS_SIGNATURE") {
+									alert('Sorry, Wrong AccessSignature received!');
+								} else if(data['error'] == 'WRONG_PLAYER_ID') {
+									alert('Sorry, Wrong Player ID received!');
+								} else if(data['error'] == 'JSON_PARSE_ERROR') {
+									alert('Sorry, Wrong JSON string format received!');
+								} else {
+									console.log(data);
+									// 1. Get all the match information into the matchInfo variable.
+									matchInfo.playerIds = data['playerIds'];
+									matchInfo.playerIdThatHasTurn = data['playerIdThatHasTurn'];
+									matchInfo.gameOverScores = data['gameOverScores'];
+									matchInfo.gameOverReason = data['gameOverReason'];
+									matchInfo.history = data['history'];
+
+									// 2. Set certain information to $scope.
+									$scope.matchInfo.playerIds = matchInfo.playerIds;
+									lastMovePlayerId = $scope.matchInfo.playerIdThatHasTurn;
+									$scope.matchInfo.playerIdThatHasTurn = matchInfo.playerIdThatHasTurn;
+								}
+							}
+					);
+				}
+
+				/**
+				 * Method used to send converted moves to server.
+				 */
+				var sendMoveToServer = function(operations){
+					if(operations.length == 1 && operations[0]['type'] == 'GameReady') {
+						// If we get "GameReady" operation, no need to send it to the server.
+						return;
+					}
+					// 1. make up a move.
+					var move = {
+						"accessSignature" : $scope.accessSignature,
+						"playerIds" : $scope.matchInfo.playerIds,
+						"operations" : operations
+					};
+					console.log(move);
+					var jsonMove = angular.json(move);
+					// 2. send jsonfied data to server.
+					MatchService.save({matchId: $routeParams.matchId}, jsonMove).
+							$promise.then(function(data) {
+								console.log(data);
+								if(data['error'] == "WRONG_ACCESS_SIGNATURE") {
+									alert('Sorry, Wrong Access Signature received!');
+								} else if(data['error'] == 'WRONG_PLAYER_ID') {
+									alert('Sorry, Wrong Player ID received!');
+								} else if(data['error'] == "JSON_PARSE_ERROR") {
+									alert('Sorry, Wrong JSON Format received!');
+								} else if(data['error'] == "MISSING_INFO") {
+									alert('Sorry, Incompleted JSON data received!');
+								} else {
+									sentMoveReceivedData = data['state'];
+								}
+							}
+					);
+				}
+
+				/*
+				 Helper function used to get sce trusted url
+				  */
 				var sceTrustedUrl = function(url){
 					return $sce.trustAsResourceUrl(url);
 				}
 
-				// object used to store all the information of the game and matches.
-				$scope.gameInfo = {};
-				$scope.matchInfo = {};
-				$scope.operations = [
-					{
-						"value":"sd",
-						"type":"Set",
-						"visibleToPlayerIds":"ALL",
-						"key":"k"
-					}
-				];
-				var move = {};
-
-				GetGameInfoService.get({gameId: $cookies.gameId}).
-						$promise.then(function(data) {
-							$scope.gameInfo.url = sceTrustedUrl(data['url']);
-							$scope.gameInfo.height = data['height'];
-							$scope.gameInfo.width = data['width'];
-						}
-				);
-
-				MatchService.get({matchId: $routeParams.matchId, accessSignature: $cookies.accessSignature, playerId: $cookies.playerId}).
-						$promise.then(function(data){
-							$scope.matchInfo.playerIds = data['playerIds'];
-							$scope.matchInfo.playerThatHasTurn = data['playerThatHasTurn'];
-							$scope.matchInfo.gameOverScores = data['gameOverScores'];
-							$scope.matchInfo.gameOverReason = data['gameOverReason'];
-							$scope.matchInfo.history = data['history'];
-						}
-				);
-
-				// method used to send converted moves to server.
-				var sendMoveToServer = function(operations){
-					var move = {};
-					move.accessSignature = $cookies.accessSignature;
-					move.playerIds = [$cookies.playerId, $cookies.friendId];
-					if((typeof operations == "object")){
-						move.operations = [operations];
-					}else{
-						move.operations = operations;
-					}
-					move = angular.toJson(move);
-					console.log(move);
-
-					MatchService.save({matchId: $routeParams.matchId}, move).
-							$promise.then(function(data) {
-								console.log(data);
-							});
+				/**
+				 * Method used to end current game in two situation:
+				 * 1. Game has a winner.
+				 * 2. One of the players surrenders.
+				 */
+				var endGame = function() {
+					socket = channel.close();
+					$location.url('/');
 				}
 
-				// method used to reload the match page to get the new match information for the server.
-				$scope.reload = function() {
-					$route.reload();
-				}
-
-				var hardCodeState = {"type" : "StartGame", "state": "gameState", "yourPlayerId" : 1, "playerIds" : [0, 1]};
-
-				$scope.sendMessage = function(){
-					var win = $window.document.getElementById('iframe1').contentWindow;
-					win.postMessage(hardCodeState, "*");
-				};
-
-				if($window.addEventListener){
-					addEventListener("message", listener, false);
-				}else{
-					attachEvent("onmessage", listener);
-				};
-
-				function listener(event) {
-					var data = event.data;
-					sendMoveToServer(data);
-
-					if(angular.isUndefined($scope.debug)){
-						$scope.debug = "Received: " + JSON.stringify(data);
-					}else{
-						$scope.operations = data;
-						$scope.debug += "Received: " + JSON.stringify(data);
-					}
-					$scope.$apply();
-				};
+				/*
+				 Code for Yuanyi Yang.
+				 */
 			}
 		}
 );
