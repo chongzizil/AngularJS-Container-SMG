86b450f792903cd7d5eff338e9f6258eeafaa750
chongzizil <chongzizil@gmail.com>
2014-04-06 13:07:24 -0400

    Updated the lobby controller for V3's two modes.

--- app/js/controllers/lobbyController.js
+++ app/js/controllers/lobbyController.js
@@ -1,29 +1,53 @@
 'use strict';
 
-smgContainer.controller('LobbyController',
-		function ($scope, $rootScope, $routeParams, $location, $cookies, $timeout,
-		          joinQueueService, InsertMatchService, GetGameInfoService) {
+smgContainer.controller('LobbyController', function (
+		$scope, $rootScope, $routeParams, $location, $cookies,
+		$timeout, joinQueueService, NewMatchService) {
 
-			if ($cookies.playerId === "Guest" || $cookies.accessSignature === null){
-				var urlData = $location.search();
-				if (urlData['playerId']) {
-					$cookies.playerId = urlData['playerId'];
-					$cookies.accessSignature = urlData['accessSignature'];
-					$rootScope.refreshDisplayId();
-				}
+			// Alerts
+			var inviteAlert = $("#inviteAlert");
+			inviteAlert.on('close.bs.alert', function() {
+				inviteAlert.hide();
+				return false;
+			})
+			// Alerts
+			var noNewMatchAlert = $("#noNewMatchAlert");
+			noNewMatchAlert.on('close.bs.alert', function() {
+				noNewMatchAlert.hide();
+				return false;
+			})
+			// Alerts
+			var needLoginAlert = $("#needLoginAlert");
+			needLoginAlert.on('close.bs.alert', function() {
+				needLoginAlert.hide();
+				return false;
+			})
+			// Alerts
+			var autoMatching = $("#autoMatching");
+
+
+
+			// Check the login is woring
+			console.log("cookies.playerId: " + $cookies.playerId);
+			console.log("cookies.accessSignature: " + $cookies.accessSignature);
+
+			// Initial the mode check
+			$cookies.isSyncMode = false;
+
+			// If the login info is contained in the url, then retrieve the login data
+			var urlData = $location.search();
+			if (urlData['playerId'] != undefined && urlData['accessSignature'] != undefined){
+				$cookies.playerId = urlData['playerId'];
+				$cookies.accessSignature = urlData['accessSignature'];
+				$rootScope.refreshDisplayId();
 			}
 
-			/**
-			 * If the player is not login yet, a window will pop up for him/her to login
-			 */
+			//If the player is not login yet, a popup will alert him/her to login first
 			if ($cookies.playerId === "Guest" || $cookies.accessSignature === null) {
-				var needLoginAlert = $("#needLoginAlert");
-				needLoginAlert.on('close.bs.alert', function() {
-					needLoginAlert.hide();
-					return false;
-				})
+				// Show the modal to alert the player
 				needLoginAlert.modal('show');
 
+				// A simple count down for login page pop up
 				$scope.timer = 3;
 				$scope.countDown = function(){
 					$scope.timer--;
@@ -36,14 +60,12 @@ smgContainer.controller('LobbyController',
 				}
 
 				var myTimer = $timeout($scope.countDown,1000);
-
 			} else {
+				// If the player login, then retrieve the login info
 				var accessSignature = $cookies.accessSignature;
 				var playerId = $cookies.playerId;
 				var gameId = $routeParams.gameId;
 			}
-
-
 
 			/**
 			 * Insert a match when one player receive the playerIds
@@ -56,7 +78,7 @@ smgContainer.controller('LobbyController',
 					gameId: gameId
 				};
 				var jsonData = angular.toJson(Data);
-				InsertMatchService.save({}, jsonData).
+				NewMatchService.save({}, jsonData).
 						$promise.then(function(data) {
 							console.log(data);
 							/*
@@ -96,6 +118,44 @@ smgContainer.controller('LobbyController',
 			}
 
 			/**
+			 * Try to retrieve the match info if there is one for the player
+			 */
+			var checkHasNewMatch = function() {
+				NewMatchService.get({playerId: $cookies.playerId, accessSignature: $cookies.accessSignature}).
+						$promise.then(function(data) {
+							console.log(data);
+							/*
+							 {@code data} contains following data if there's a match:
+							 matchId:
+							 playerIds: should be an array, and we can store this into the $cookies and
+							 delete it after all players exit the match.
+							 */
+							if(!data['matchId']) {
+								if (data['error'] === 'WRONG_ACCESS_SIGNATURE') {
+									alert('Sorry, your ID does not exist. Please try again.');
+									$("#inviteAlert").show();
+								} else if (data['error'] === 'WRONG_PLAYER_ID') {
+									alert('Sorry, your ID does not exist. Please try again.');
+								} else if (data['error'] === 'NO_MATCH_FOUND') {
+									noNewMatchAlert.show();
+								}
+							} else {
+								$("#autoMatching").hide();
+								// Store the playerIds and matchId in the cookies
+								$cookies.playerIds = data['playerIds'];
+								$cookies.matchId = data['matchId'];
+								$location.url(gameId + '/match/' + data['matchId']);
+								if(!$scope.$$phase) {
+									$scope.$apply();
+								}
+							}
+						}
+				);
+			}
+
+
+
+			/**
 			 * Start a auto match by using channel API
 			 */
 			$scope.autoMatch = function() {
@@ -108,16 +168,22 @@ smgContainer.controller('LobbyController',
 				// Change the data to json object
 				var jsonJoinQueueData = angular.toJson(joinQueueData);
 
-				$("#inviteAlert").hide();
+				inviteAlert.hide();
+				noNewMatchAlert.hide();
 
 				// functions for the socket
 				var onopen = function () { console.log("channel opened..."); };
 				var onerror = function () { };
 				var onclose =function () { console.log("channel closed..."); };
 				var onmessage = function (event) {
-					$("#autoMatching").show();
+					autoMatching.show();
+
+					// Sync mode is choosed
+					$cookies.isSyncMode = true;
+
 					// Receive the data from the channel
 					var data = angular.fromJson(event.data);
+
 					if (data['matchId']) {
 						$cookies.playerIds = data['playerIds'];
 						// Jump to the game page to start playing :
@@ -163,22 +229,21 @@ smgContainer.controller('LobbyController',
 			 * Start a match by inviting a friend
 			 */
 			$scope.invite = function(inviteInfo) {
-				var accessSignature = $cookies.accessSignature;
+				// Retrieve the friend's Id
 				var friendId = inviteInfo.friendId;
+
+				// Async mode is choosed
+				$cookies.isSyncMode = false;
 
 				// Initiate the alert
 				$scope.friendIdHasError = false;
 
-				var inviteAlert = $("#inviteAlert");
-				inviteAlert.on('close.bs.alert', function() {
-					inviteAlert.hide();
-					return false;
-				})
+				// Cancel the auto match
+				$scope.cancel();
+				autoMatching.hide();
 
 				var playerIds = [playerId, friendId];
 
 				insertMatch(playerIds);
 			 }
-
-
 		});
