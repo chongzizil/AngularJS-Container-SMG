c00cd8a9d4e6a6112bd4cda20f00ec0b408e2e3d
chongzizil <chongzizil@gmail.com>
2014-04-15 00:57:00 -0400

    Fixed a bug in menuCtrl.

--- app/js/controllers/menuController.js
+++ app/js/controllers/menuController.js
@@ -1,24 +1,24 @@
 'use strict';
 
 smgContainer.controller('MenuController',
-    function ($scope, $cookies, $rootScope, $location, GetTokenFromFBService) {
+    function ($scope, $cookies, $rootScope, $location) {
 
-	    // If the login info is contained in the url, then retrieve the login data
-	    var urlData = $location.search();
-	    if (urlData['code'] != undefined) {
-		    $cookies.code = urlData['code'];
-				console.log("Code received: " + $cookies.code);
-				var code = "AQAg61TQOYqtKKo-b4CoE7-ZxdkS7lGIyQJs_WS4JQix8ffRUltTc0byovrBMvfQCRGWOOjWPmKts9r95Kk0wVkgKryDGj-YPulkFIQN1sKhqJ85RMxy0XGKZOjm9giDdndYKybNcq-cUVLs7hErShfpu6SOYgKVeKjQMq0adXIufgS71o8itqD_GNCdNM0GHWSe_mRTJctcJYTGq1Oe3LBAvJo0mzemyq5wCkDK1Weat6fert8MokTCoqu_xq8brBKJOnSgj9dUdjah1jbAohENbJSnPAF4aj8-fW-Dr6QmGCFJHemZ-YifYtofmQN_n4E";
-		    GetTokenFromFBService.get({code: code}).
-				    $promise.then(function (data) {
-					    if (!data['access_token']) {
-						    alert(data);
-					    } else {
-						    $cookies.accessToken = data['access_token'];
-					    }
-				    }
-		    );
-	    }
+//	    // If the login info is contained in the url, then retrieve the login data
+//	    var urlData = $location.search();
+//	    if (urlData['code'] != undefined) {
+//		    $cookies.code = urlData['code'];
+//				console.log("Code received: " + $cookies.code);
+//				var code = "AQAg61TQOYqtKKo-b4CoE7-ZxdkS7lGIyQJs_WS4JQix8ffRUltTc0byovrBMvfQCRGWOOjWPmKts9r95Kk0wVkgKryDGj-YPulkFIQN1sKhqJ85RMxy0XGKZOjm9giDdndYKybNcq-cUVLs7hErShfpu6SOYgKVeKjQMq0adXIufgS71o8itqD_GNCdNM0GHWSe_mRTJctcJYTGq1Oe3LBAvJo0mzemyq5wCkDK1Weat6fert8MokTCoqu_xq8brBKJOnSgj9dUdjah1jbAohENbJSnPAF4aj8-fW-Dr6QmGCFJHemZ-YifYtofmQN_n4E";
+//		    GetTokenFromFBService.get({code: code}).
+//				    $promise.then(function (data) {
+//					    if (!data['access_token']) {
+//						    alert(data);
+//					    } else {
+//						    $cookies.accessToken = data['access_token'];
+//					    }
+//				    }
+//		    );
+//	    }
 
       $rootScope.refreshDisplayId = function () {
         $scope.idDisplay = 'Guest';
